79cf6dc7351d2b1bceaea1819e0bab7527893aba
Eren Küpeli <eren_kpl@hotmail.com>
2025-04-14 11:55:53 +0300

    bug fixing

--- js/iletisim-script.js
+++ js/iletisim-script.js
@@ -8,10 +8,13 @@ const sonucDiv = document.getElementById("sonuc");
 form.addEventListener("submit", function(e){
     e.preventDefault();
 
-    const isim = isimInput.nodeValue;
-    const mesaj = mesajInput.nodeValue;
+    console.log(isimInput);
+    console.log(isimInput.value);
+
+    /*const isim = isimInput.value;
+    const mesaj = mesajInput.value;
 
     sonucDiv.innerHTML = '<p><strong>Teşekkürler, ${isim}!</strong></p><p>Mesajınız: ${mesa}</p>';
 
-    form.reset();
+    form.reset();*/
 });
